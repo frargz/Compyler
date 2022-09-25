83e9553b4a1ed0409c5b55d060fbde3790621d07
frargz <danielchappell252@gmail.com>
2022-09-25 08:16:22 -0700

    aut

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
 
 //const {exec} = require('child_process');
 
@@ -13,11 +15,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api', (req, res) => {
+    data = req.body
     console.log(req);
     res.end();
 });
 
 const PORT = process.env.PORT;
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`server is running on this port ${PORT}`);
 });
