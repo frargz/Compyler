c724d8ac6f07d90a5a77baf151655136c6e6b683
frargz <danielchappell252@gmail.com>
2022-09-25 06:53:35 -0700

    aut

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.get('/endpoint-2', (request, response, next) => {
 //    res.end();
 //});
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server is running on this port ${PORT}`));
