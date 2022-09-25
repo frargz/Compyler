ca3c6fb6de06c0f3a3a351ec9b02052e354bcb5c
frargz <danielchappell252@gmail.com>
2022-09-25 05:44:31 -0700

    post

--- index.js
+++ index.js
@@ -19,9 +19,14 @@ app.get('/endpoint-2', (request, response, next) => {
         success: true,
         data: {
             message: 'Hello from endpoint 2'
-        }
+   }
     })
 })
+
+app.post('/api', (req, res) => {
+    console.log(req);
+    res.end();
+});
 
 const PORT = process.env.PORT || 50001;
 app.listen(PORT, () => console.log(`server is running on this port ${PORT}`));
